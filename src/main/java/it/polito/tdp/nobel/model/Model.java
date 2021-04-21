8181b1b5c8ab5f375d024ea25600e55d3e1a32a8
elena <elena@localhost>
2021-04-21 21:59:59 +0200

    implementata

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -7,8 +7,9 @@ import java.util.Set;
 import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
-	private List<Esame> partenza;
-	private Set<Esame> soluzioneMigliore;
+	private List<Esame> partenza; //tutti gli esami anche duplicati
+	private Set<Esame> soluzioneMigliore; //solo alcuni e non ripetuti (ordine non conta)
+											// migliore tra le parziali
 	private double mediaSoluzioneMigliore;
 	
 	public Model() {
@@ -28,12 +29,16 @@ public class Model {
 
 	/*COMPLESSITA': 2^N*/
 	private void cerca2(Set<Esame> parziale, int L, int m) {
-		//casi terminali
+		
+		//**********casi terminali *********************
+		
+		// 1 : m crediti sforati
 		int crediti = sommaCrediti(parziale);
 		if(crediti > m) {
 			return;
 		}
-				
+			
+		// 2 : sono precisamente a "m" e la media è > della migliore fino ad ora
 		if(crediti == m) {
 			double media = calcolaMedia(parziale);
 			if(media > mediaSoluzioneMigliore) {
@@ -43,13 +48,17 @@ public class Model {
 			return; 
 		}
 				
+		// 3
 		//sicuramente, crediti < m
 		// L = N -> non ci sono più esami da aggiungere
 		if(L == partenza.size()) {
 			return;
 		}
-		//generazione sottoproblemi
+		
+		//********** generazione sottoproblemi*****************
+		// FA LA DIFFERENZA IN COMPLESSITA' RISPETTO A PRIMA 
 		//partenza[L] è da aggiungere oppure no? Provo entrambe le cose
+		//
 		parziale.add(partenza.get(L));
 		cerca2(parziale, L+1, m);
 		
@@ -60,6 +69,8 @@ public class Model {
  
 
 	/*COMPLESSITA': N!*/
+	// quando aumento N non riesce a runnare neanche con 50
+	// nel caso di prima di metto 39 ms
 	private void cerca1(Set<Esame> parziale, int L, int m) {
 		//casi terminali
 		int crediti = sommaCrediti(parziale);
@@ -83,14 +94,46 @@ public class Model {
 		}
 		
 		//generare i sotto-problemi
-		for(Esame e : partenza) {
+		/*for(Esame e : partenza) {
 			if(!parziale.contains(e)) {
+				//aggiungo
 				parziale.add(e);
+				//ricorsione
 				cerca1(parziale, L+1, m);
+				//backtracking
 				parziale.remove(e);
 			}
+		}*/
+		
+		//N.B. : IMPLEMENTAZIONE: indice del for deve essere > del livello di ricorsione
+		// non ha senso controllare indici precedenti al valore del livello, perché è già pieno
+		for(int i=0;i<partenza.size();i++) {
+			if(!parziale.contains(partenza.get(i))&& i>=L) {
+				parziale.add(partenza.get(i));
+				cerca1(parziale,L+1,m);
+				parziale.remove(partenza.get(i));
+			}
 		}
+		
+		/*
+		 * partenza = {e1,e2,e3}
+		 * 
+		 * LIV=0
+		 * parziale={e1}
+		 * 		L=1
+		 * 		parziale={e1,e2}
+		 * parziale={e2}
+		 * 		L=1
+		 * 		parziale={e2,e1} --> non considero più questa soluzione
+		 * 							perché e1 viene prima di e1 ha indice 0
+		 * 							e il liv è 1, quindi questo mi permette
+		 * 							di evitare il duplicato della riga 124
+		 * 							dove i valori erano solo invertiti
+		 * parziale ={e3}
+		 */
+		
 	}
+	
 
 	public double calcolaMedia(Set<Esame> esami) {
 		
